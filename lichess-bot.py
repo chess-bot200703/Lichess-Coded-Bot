627a61e67645dc8c44529676b9326a80a7b6aef5
2022FIFA <99064634+2022FIFA@users.noreply.github.com>
2022-02-07 20:55:03 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -236,6 +236,7 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     conversation.send_reply(SendLine('player'), f'Good Luck {opponent}!')
     conversation.send_reply(SendLine('player'), f'Type #help for list of commands I can Reply to.Thanks for playing with me')
     conversation.send_reply(SendLine('spectator'), f'Thanks for spectating me!')
+    conversation.send_reply(SendLine('spectator'), f'Type #help for list of commands I can reply to!')
     
     variant=game.perf_name
       
