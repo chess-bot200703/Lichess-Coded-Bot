7692ae4bfca46ba5cca196635162ec147149ac9f
chess-bot200703 <99081475+chess-bot200703@users.noreply.github.com>
2022-02-24 20:52:17 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -233,8 +233,8 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         def __init__(self, room):
             self.room = room
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
-    conversation.send_reply(SendLine('player'), f'Good luck {opponent}!')
-    conversation.send_reply(SendLine('spectator'), f'Good luck {opponent}!')
+    conversation.send_reply(SendLine('player'), f'Programmed by @Javan727 and @EshanHasaranga200703,Good luck {opponent}!')
+    conversation.send_reply(SendLine('spectator'), f'Programmed by @Javan727 and @EshanHasaranga200703,Type !help for get list of commands I can respond to')
     
     variant=game.perf_name
       
